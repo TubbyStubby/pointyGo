37774c088ead9455c3c502b18694dcccffa647c2
TubbyStubby <tubbystubby2@gmail.com>
2020-11-09 16:16:19 +0530

    Added articles route

--- main.go
+++ main.go
@@ -3,56 +3,81 @@ package main
 import (
 	"context"
 	"encoding/json"
-	"fmt"
+	"log"
 	"net/http"
 	"os"
 	"time"
 
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
+type Article struct {
+	ID        primitive.ObjectID `bson:"_id,omitempty"`
+	Title     string             `bson:"title,omitempty"`
+	Subtitle  string             `bson:"subtitle,omitempty"`
+	Content   string             `bson:"content,omitempty"`
+	Timestamp primitive.DateTime `bson:"timestamp,omitempty"`
+}
+
+var client *mongo.Client
+var articlesCollection *mongo.Collection
+var ctx context.Context
+var err error
+
+type Router struct {
+}
+
 func main() {
-	resp, err := http.Get("https://api.stackexchange.com/2.2/questions?pagesize=100&order=desc&sort=activity&tagged=go&site=stackoverflow")
+	ctx, _ = context.WithTimeout(context.Background(), 10*time.Second)
+	client, err = mongo.Connect(ctx, options.Client().ApplyURI(os.Getenv("TEST_ATLAS_URI")))
 	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
-
-	var result map[string][]map[string]interface{}
-	json.NewDecoder(resp.Body).Decode(&result)
-
-	list := result["items"]
-
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI(os.Getenv("TEST_ATLAS_URI")))
-	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 	defer client.Disconnect(ctx)
 
-	if err := client.Ping(ctx, readpref.Primary()); err != nil {
-		panic(err)
-	}
+	articlesCollection = client.Database("pointyGo").Collection("articles")
 
-	pGodb := client.Database("pointyGo")
-	someClcn := pGodb.Collection("articles")
-
-	for i, v := range list {
-		doc := bson.D{
-			{"title", v["title"]},
-			{"subtitle", fmt.Sprintf("Views: %.0f", v["view_count"])},
-			{"content", fmt.Sprintf("Lorem ipsum content here.  %d", i)},
-			{"timestamp", time.Now()},
-		}
-
-		_, err := someClcn.InsertOne(ctx, doc)
-		if err != nil {
-			panic(err)
-		}
-
-		fmt.Printf("Inserted %v into collection.\n", i)
-	}
+	mux := &Router{}
+	http.ListenAndServe(":9090", mux)
 }
+
+func (p *Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch path := r.URL.Path; path {
+	case "/articles":
+		fetchArticles(w, r)
+	default:
+		http.NotFound(w, r)
+	}
+	return
+}
+
+func fetchArticles(resp http.ResponseWriter, req *http.Request) {
+	ctx, _ = context.WithTimeout(context.Background(), 20*time.Second)
+
+	resp.Header().Add("content-type", "application/json")
+
+	filterCursor, err := articlesCollection.Find(ctx, bson.M{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer filterCursor.Close(ctx)
+
+	var articles []Article
+	if err = filterCursor.All(ctx, &articles); err != nil {
+		log.Fatal(err)
+	}
+
+	json.NewEncoder(resp).Encode(articles)
+}
+
+// fmt.Println("Found,")
+// for filterCursor.Next(ctx) {
+// 	var article Article
+// 	if err := filterCursor.Decode(&article); err != nil {
+// 		panic(err)
+// 	}
+// 	fmt.Print(article.Title, "\n")
+// }
