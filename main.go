57352e0823fd8bac81ea5a126c9c5fab014ab339
TubbyStubby <tubbystubby2@gmail.com>
2020-11-09 21:03:04 +0530

    Added search

--- main.go
+++ main.go
@@ -6,16 +6,19 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"regexp"
+	"strings"
 	"time"
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/x/bsonx"
 )
 
 type Article struct {
-	ID        primitive.ObjectID `bson:"_id,omitempty"`
+	Id        primitive.ObjectID `bson:"_id,omitempty"`
 	Title     string             `bson:"title,omitempty"`
 	Subtitle  string             `bson:"subtitle,omitempty"`
 	Content   string             `bson:"content,omitempty"`
@@ -25,6 +28,7 @@ type Article struct {
 var client *mongo.Client
 var articlesCollection *mongo.Collection
 var err error
+var index mongo.IndexModel
 
 type Router struct {
 }
@@ -39,24 +43,69 @@ func main() {
 
 	articlesCollection = client.Database("pointyGo").Collection("articles")
 
+	index = mongo.IndexModel{
+		Options: options.Index().SetBackground(true),
+		Keys: bsonx.MDoc{
+			"title":    bsonx.String("text"),
+			"content":  bsonx.String("text"),
+			"subtitle": bsonx.String("text"),
+		},
+	}
+	if _, err := articlesCollection.Indexes().CreateOne(ctx, index); err != nil {
+		log.Print(err)
+	}
+
 	mux := &Router{}
 	http.ListenAndServe(":9090", mux)
 }
 
+func checkString(s string, rs string) bool {
+	return regexp.MustCompile(rs).MatchString(s)
+}
+
 func (p *Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	switch path := r.URL.Path; path {
-	case "/articles":
+	r.ParseForm()
+	switch {
+	case checkString(r.URL.Path, `^/articles/?$`):
 		fetchArticles(w, r)
-	case "/articles/":
-		id := path[len("/articles/"):]
+
+	case checkString(r.URL.Path, `/articles/search?.*`):
+		query := r.Form["q"]
+		searchArticles(w, r, query)
+
+	case checkString(r.URL.Path, `/articles/[a-zA-z0-9]*`):
+		id := r.URL.Path[len("/articles/"):]
 		fetchArticles(w, r, id)
+
 	default:
 		http.NotFound(w, r)
 	}
 	return
 }
 
-func fetchArticles(resp http.ResponseWriter, req *http.Request, id ...string) {
+func searchArticles(resp http.ResponseWriter, req *http.Request, query []string) {
+	ctx, _ := context.WithTimeout(context.Background(), 20*time.Second)
+	resp.Header().Add("content-type", "application/json")
+
+	indexCursor, err := articlesCollection.Find(ctx, bson.M{
+		"$text": bson.M{
+			"$search": strings.Join(query, " "),
+		},
+	})
+	if err != nil {
+		log.Print(err)
+	}
+
+	var articles []Article
+
+	if err = indexCursor.All(ctx, &articles); err != nil {
+		log.Print(err)
+	}
+
+	json.NewEncoder(resp).Encode(articles)
+}
+
+func fetchArticles(resp http.ResponseWriter, req *http.Request, sid ...string) {
 	ctx, _ := context.WithTimeout(context.Background(), 20*time.Second)
 	resp.Header().Add("content-type", "application/json")
 
@@ -64,7 +113,8 @@ func fetchArticles(resp http.ResponseWriter, req *http.Request, id ...string) {
 	case "GET":
 		var filterCursor *mongo.Cursor
 		var err error
-		if len(id) > 0 {
+		if len(sid) > 0 {
+			id, _ := primitive.ObjectIDFromHex(sid[0])
 			filterCursor, err = articlesCollection.Find(ctx, bson.M{"_id": id})
 		} else {
 			filterCursor, err = articlesCollection.Find(ctx, bson.M{})
